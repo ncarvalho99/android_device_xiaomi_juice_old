71ca9b25363212d3b48cdb8b3308f5ff6b04cc3b
hunterxznuub <90485970+hunterxznuub@users.noreply.github.com>
2021-09-28 11:49:48 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_juice.mk
+    $(LOCAL_DIR)/aicp_juice.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_juice-user \
-    aosp_juice-userdebug \
-    aosp_juice-eng
+    aicp_juice-user \
+    aicp_juice-userdebug \
+    aicp_juice-eng
